c04b207ea3ef5ea2063a3f82da71175f93c71e6a
Sriya Pandey <sriyapandey0405@gmail.com>
2025-04-03 18:42:19 +0530

    Create checkIfTheArrayIsSorted.cpp

---
+++ checkIfTheArrayIsSorted.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool arraySortedOrNot(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = 1 ; i<n ; i++){
+            if(arr[i]>=arr[i-1]){
+                
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
