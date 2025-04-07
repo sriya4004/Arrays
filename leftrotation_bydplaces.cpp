6dbe46610345353e29a8a64d78243338d67862e4
Sriya Pandey <sriyapandey0405@gmail.com>
2025-04-07 15:29:24 +0530

    Update leftrotation_bydplaces.cpp

--- leftrotation_bydplaces.cpp
+++ leftrotation_bydplaces.cpp
@@ -1,3 +1,26 @@
+//Brute Force
+void leftRotate(int arr[], int n, int d) {
+    d = d % n;  // In case d > n
+    int temp[d];
+
+    // Step 1: Store first d elements in temp[]
+    for (int i = 0; i < d; i++) {
+        temp[i] = arr[i];
+    }
+
+    // Step 2: Shift the remaining elements to the left
+    for (int i = d; i < n; i++) {
+        arr[i - d] = arr[i];
+    }
+
+    // Step 3: Copy elements from temp[] to the end
+    for (int i = n - d; i < n; i++) {
+        arr[i] = temp[i - (n - d)];
+    }
+}
+
+
+
 //optimal
 class Solution {
 public:
